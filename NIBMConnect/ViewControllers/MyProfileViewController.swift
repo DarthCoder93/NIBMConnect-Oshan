2b7779536f4f1509677b14684f11d62101ddc960
Oshan <oshan@Oshans-MacBook-Pro.local>
2019-05-23 07:26:36 +0530

    biometrics authntication added

--- NIBMConnect/ViewControllers/MyProfileViewController.swift
+++ NIBMConnect/ViewControllers/MyProfileViewController.swift
@@ -35,44 +35,82 @@ class MyProfileViewController: UIViewController {
     }
     
     
-    func authenticateUser() {
-        let myContext = LAContext()
-        let myLocalizedReasonString = "Biomatric authentication needs to be provided to view this page"
+    func authenticateUser(){
+        let authContext = LAContext()
+        let authReason = "Please use Touch ID to access Your Account Data"
+        var authError : NSError?
         
-        var authError: NSError?
-        if #available(iOS 8.0, macOS 10.12.1, *) {
-            if myContext.canEvaluatePolicy(.deviceOwnerAuthentication, error: &authError) {
-                myContext.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: myLocalizedReasonString) { success, evaluateError in
-                    
+        if authContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &authError){
+            
+            authContext.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: authReason, reply: { (success,error) -> Void in
+                if success{
+                    //Go to My Account Page
                     DispatchQueue.main.async {
-                        if success {
-                            // User authenticated successfully, take appropriate action
-                            self.showUserData()
-                            
-                        } else {
-                            
-                            let err = evaluateError?.localizedDescription
-                            
-                            if (err != nil) {
-                                self.alert(message: err!, title: "Error", callback: {
-                                    self.navigationController?.popViewController(animated: true)
-                                })
-                            }
-                            
-                        }
+                         self.showUserData()
                     }
                 }
-            } else {
-                self.alert(message: "Authentication Failed", callback: {
-                     self.navigationController?.popViewController(animated: true)
-                })
-            }
-        } else {
+                
+                if error != nil{
+                    let errMessage = error?.localizedDescription
+                    
+                    if (errMessage != nil) {
+                        self.alert(message: errMessage!, title: "Error", callback: {
+                            self.navigationController?.popViewController(animated: true)
+                        })
+                    } else {
+                        self.navigationController?.popViewController(animated: true)
+                    }
+                }
+                
+            })
+            
+        } else{
             self.alert(message: "Authentication Failed", callback: {
                 self.navigationController?.popViewController(animated: true)
             })
         }
     }
+    
+    
+    
+//    func authenticateUser() {
+//        let myContext = LAContext()
+//        let myLocalizedReasonString = "Biomatric authentication needs to be provided to view this page"
+//
+//        var authError: NSError?
+//        if #available(iOS 8.0, macOS 10.12.1, *) {
+//            if myContext.canEvaluatePolicy(.deviceOwnerAuthentication, error: &authError) {
+//                myContext.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: myLocalizedReasonString) { success, evaluateError in
+//
+//                    DispatchQueue.main.async {
+//                        if success {
+//                            // User authenticated successfully, take appropriate action
+//                            self.showUserData()
+//
+//                        } else {
+//
+//                            let err = evaluateError?.localizedDescription
+//
+//                            if (err != nil) {
+//                                self.alert(message: err!, title: "Error", callback: {
+//                                    self.navigationController?.popViewController(animated: true)
+//                                })
+//                            }
+//
+//                        }
+//                    }
+//                }
+//            } else {
+//                self.alert(message: "Authentication Failed", callback: {
+//                     self.navigationController?.popViewController(animated: true)
+//                })
+//            }
+//        } else {
+//            self.alert(message: "Authentication Failed", callback: {
+//                self.navigationController?.popViewController(animated: true)
+//            })
+//        }
+//    }
     
     
     private func showUserData() {
